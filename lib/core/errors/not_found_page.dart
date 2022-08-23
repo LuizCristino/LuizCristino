e309e242a1641f6fcbaa4c3952755d2ebdf5b6e0
Luiz Cristino <luiz.cristino@underwork.com.br>
2022-08-23 17:39:49 -0400

    Add not found page

--- lib/core/errors/not_found_page.dart
+++ lib/core/errors/not_found_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 /// Page when url is invalid
 class NotFoundPage extends StatelessWidget {
@@ -7,6 +8,41 @@ class NotFoundPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('404 Not found!')));
+    final textColor = Theme.of(context).textTheme.bodyText1!.color;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '404 - Not found',
+              style: TextStyle(fontSize: 32, letterSpacing: 1.25),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "We didn't find your page,",
+              style: TextStyle(fontSize: 24, letterSpacing: 1.25),
+            ),
+            const Text(
+              'but found you a cat!',
+              style: TextStyle(fontSize: 24, letterSpacing: 1.25),
+            ),
+            Lottie.asset('assets/lotties/sleeping_cat.json'),
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              style: ElevatedButton.styleFrom(
+                onPrimary: textColor,
+                padding: const EdgeInsets.all(16),
+              ),
+              child: const Text(
+                'Go Back',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
