ec27a5a2f34dee5a502e036b65ddbca58853b930
Luiz Cristino <luizptcristino@gmail.com>
2022-08-24 08:34:56 -0400

    Remove sharp (#) from url

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:github_pages/config/routers/app_router.gr.dart';
 import 'package:github_pages/config/themes/dark_theme.dart';
 import 'package:github_pages/config/themes/light_theme.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
