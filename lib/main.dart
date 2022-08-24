09804b2084279d8c1088a1652a60b18ba5b1bc46
Luiz Cristino <luiz.cristino@underwork.com.br>
2022-08-24 08:10:29 -0400

    Correct display name of app and pwa

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      title: 'Luiz Cristino',
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
       builder: (context, router) => router!,
